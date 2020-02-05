e5d04665048a61605f2f0d400a491495d7aa626c
erik-sistos <60245923+erik-sistos@users.noreply.github.com>
2020-02-04 19:49:35 -0600

    Add files via upload

---
+++ EAS-prg1-BusinessCard/EAS-prg1-BusinessCard/EAS-prg1-BusinessCard.cpp
@@ -0,0 +1,13 @@
+// EAS-prg1-BusinessCard.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to ITSE 1307!\n";
+    std::cout << "Main Street Pizza & Pasta\n";
+    std::cout << "Phone: 210-732-8861\n";
+    std::cout << "Address: 1906 N Main Ave, San Antonio, TX, 78212\n";
+    std::cout << "Website: www.mainstreetsa.com\n";
+    std::cout << "Email: corky@mainstreetsa.com\n";
+}
