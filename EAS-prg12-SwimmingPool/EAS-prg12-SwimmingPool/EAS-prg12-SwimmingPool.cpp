4211c53c77770a93df38aa9a32e1f6e8e944761b
erik-sistos <60245923+erik-sistos@users.noreply.github.com>
2020-04-28 22:19:19 -0500

    Add files via upload

--- EAS-prg12-SwimmingPool/EAS-prg12-SwimmingPool/EAS-prg12-SwimmingPool.cpp
+++ EAS-prg12-SwimmingPool/EAS-prg12-SwimmingPool/EAS-prg12-SwimmingPool.cpp
@@ -10,12 +10,15 @@
 
 int main()
 {
+    //Variables
     int intLength = 0;
     int intWidth = 0;
     int intDepth = 0;
     float fltInitialVolume = 0;
     float fltFillRate = 0;
     float fltDrainageRate = 0;
+
+    //Gathers user input
     std::cout << "Enter the length, width, and depth of the pool in feet: ";
     std::cin >> intLength >> intWidth >> intDepth;
     std::cout << "Enter the amount of water already in the pool in gallons: ";
@@ -25,15 +28,18 @@ int main()
     std::cout << "Enter the drainage rate of the pool in gallons per minute: ";
     std::cin >> fltDrainageRate;
 
+    //Gathers information to put into toString function
     swimmingPool objSwimmingPoolOne = swimmingPool(intLength, intWidth, intDepth, fltInitialVolume, fltFillRate,
         fltDrainageRate);
     std::cout << "Based on the information you provided: " << objSwimmingPoolOne.toString();
 
+    //Outputs the time for the fill rate
     if (objSwimmingPoolOne.getFillRate() > 0)
     {
         std::cout << std::endl << "It will take "
             << objSwimmingPoolOne.getFillTime() << " minutes to fill" << std::endl;
     }
+    //Outputs the time for the drainage rate
     else if (objSwimmingPoolOne.getDrainageRate() > 0)
     {
         std::cout << std::endl << "It will take "
